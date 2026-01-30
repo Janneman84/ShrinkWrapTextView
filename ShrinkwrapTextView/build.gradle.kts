ab916cf016eb76f6a031ec6b0c40c7bb1faf197e
Jan de Vries <j.devries@mrchadd.nl>
2026-01-30 20:07:43 +0100

    initial commit

---
+++ ShrinkwrapTextView/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.janneman84.shrinkwraptextview"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 16
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create("release", MavenPublication::class.java) {
+                from(components.getByName("release"))
+                groupId = "Janneman84"
+                artifactId = "ShrinkWrapTextView"
+                version = "0.1.0"
+            }
+        }
+    }
+}
